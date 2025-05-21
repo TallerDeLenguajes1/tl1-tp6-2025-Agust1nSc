ff8b1dc16136251ffe3f8979b33100e09bff427a
“Agust1nSc” <agustinsaccone9@gmail.com>
2025-05-20 22:23:40 -0300

    Invertir numeros

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
 ﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+
+int i;
+int j = 0;
+int[] invertido = new int[10];  
+string numero = Console.ReadLine();
+bool resultado = int.TryParse(numero, out i);
+int copia = i;
+
+if (resultado && i > 0)
+{
+
+    while (i > 0)
+    {
+
+        i /= 10;
+
+        j++;
+    }
+    Console.WriteLine("Numero invertido: ");
+    for (int k = 0; k < j; k++)
+    {
+        invertido[k] = copia % 10;
+        Console.Write(invertido[k]);
+        copia /= 10;
+    }
+
+    
+}
